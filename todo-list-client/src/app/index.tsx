ac05b2697291f4abac195bf1bf3206fa6dc57f0f
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2025-04-18 11:46:20 +0200

    Added the routing system to the client application with React Router

--- todo-list-client/src/app/index.tsx
+++ todo-list-client/src/app/index.tsx
@@ -1,13 +1,14 @@
 import './index.scss'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
+import Router from '../router';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <></>
+      <Router />
     </StrictMode>
   )
 }
---
+++ todo-list-client/src/router/Router.component.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes as BrowserRoutes } from "react-router-dom";
+import { ApplicationRoutes } from "./Router.blocks";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <BrowserRoutes>
+                {ApplicationRoutes}
+            </BrowserRoutes>
+        </BrowserRouter>
+    );
+}
+
+export default Router;
