d81ae25ab3ecc210b0f3e99cd75095f79b115ed0
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2025-04-19 18:21:20 +0200

    Completed the forms component

---
+++ todo-list-client/src/components/auth/forms/Forms.constants.tsx
@@ -0,0 +1,13 @@
+import Login from "../login";
+import Register from "../register";
+
+export const FORM_TABS = [
+    {
+        title: "Login",
+        TabPanel: Login,
+    },
+    {
+        title: "Register",
+        TabPanel: Register,
+    },
+];
