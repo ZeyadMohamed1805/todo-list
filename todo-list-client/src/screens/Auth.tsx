491707b8edd1566858062556398150fd667a8d76
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2025-04-19 20:38:44 +0200

    Added preferences component to the auth screen

--- todo-list-client/src/screens/Auth.tsx
+++ todo-list-client/src/screens/Auth.tsx
@@ -1,11 +1,13 @@
 import Description from "../components/auth/description/Description.component";
 import Forms from "../components/auth/forms";
+import Preferences from "../components/auth/preferences";
 import Wrapper from "../components/auth/wrapper";
 
 const Auth = () => {
     return (
         <main>
             <Wrapper>
+                <Preferences />
                 <Description />
                 <Forms />
             </Wrapper>
