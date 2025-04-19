9ab0a09c1d4a70aaf08f52b602864116fc1ccbe3
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2025-04-19 02:46:07 +0200

    Added the auth screen to the routing system

--- todo-list-client/src/router/Router.constants.tsx
+++ todo-list-client/src/router/Router.constants.tsx
@@ -1,3 +1,7 @@
-import { RouteProps } from 'react-router-dom';
+import Auth from "../screens/Auth";
+import { Navigate, RouteProps } from 'react-router-dom';
 
-export const APPLICATION_ROUTES: Array<RouteProps> = [{ path: '/', Component: () => <></> }];
+export const APPLICATION_ROUTES: Array<RouteProps> = [
+    { path: '/', element: <Navigate to="/auth" replace /> },
+    { path: '/auth', Component: Auth }
+];
---
+++ todo-list-client/src/screens/Auth.tsx
@@ -0,0 +1,10 @@
+const Auth = () => {
+    return (
+        <div>
+            <h1>Auth</h1>
+            <p>This is the Auth screen.</p>
+        </div>
+    );
+};
+
+export default Auth;
