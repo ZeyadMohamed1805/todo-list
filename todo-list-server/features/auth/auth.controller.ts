0c871f6aa00dc8c1320d39397ac6cc86a0c075ab
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2025-04-23 16:17:28 +0200

    Added global status codes enum

--- todo-list-server/features/auth/auth.controller.ts
+++ todo-list-server/features/auth/auth.controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import { registerUser } from "./auth.service";
+import { StatusCodesEnum } from "../../enums/statusCodes.enum";
 
 export const register = async (request: Request, response: Response, next: NextFunction) => {
     try {
         await registerUser(request.body);
-        response.status(204).json({ success: true });
+        response.status(StatusCodesEnum.NO_CONTENT).json({ success: true });
     } catch (error) {
         next(error);
     }
--- todo-list-server/middlewares/validate.middleware.ts
+++ todo-list-server/middlewares/validate.middleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
-import { AnyObject, Maybe, ObjectSchema } from "yup";
-import { ValidationError } from "../types/error.type";
+import { AnyObject, Maybe, ObjectSchema, ValidationError as YupValidationError } from "yup";
+import { StatusCodesEnum } from "../enums/statusCodes.enum";
 
 const validate = (schema: ObjectSchema<Maybe<AnyObject>>) => {
     return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -8,12 +8,16 @@ const validate = (schema: ObjectSchema<Maybe<AnyObject>>) => {
             req.body = await schema.validate(req.body, { abortEarly: false, stripUnknown: true });
             return next();
         } catch (error) {
-            res.status(400).json({
-                errors: error.inner.map((error: ValidationError) => ({
-                    field: error.path,
-                    key: error.message,
-                })),
-            });
+            if (error instanceof YupValidationError) {
+                res.status(StatusCodesEnum.BAD_REQUEST).json({
+                    errors: error.inner.map(err => ({
+                        field: err.path,
+                        key: err.message,
+                    })),
+                });
+            } else {
+                next(error);
+            }
         }
     };
 };
