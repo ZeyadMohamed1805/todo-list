3a155fff248388e77a3fd462d3c39ee514731990
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2025-04-24 23:04:15 +0200

    Added username to registration

--- todo-list-client/src/components/auth/register/Register.constants.tsx
+++ todo-list-client/src/components/auth/register/Register.constants.tsx
@@ -1,5 +1,12 @@
 export const FORM_INPUT_FIELDS = [
   {
+    type: 'text',
+    name: 'username',
+    placeholder: 'username',
+    minLength: 8,
+    required: true,
+  },
+  {
     type: 'email',
     name: 'email',
     placeholder: 'email',
@@ -9,12 +16,14 @@ export const FORM_INPUT_FIELDS = [
     type: 'password',
     name: 'password',
     placeholder: 'password',
+    minLength: 8,
     required: true,
   },
   {
     type: 'password',
     name: 'passwordConfirmation',
     placeholder: 'confirm_password',
+    minLength: 8,
     required: true,
   },
 ];
--- todo-list-client/src/components/auth/register/Register.hooks.tsx
+++ todo-list-client/src/components/auth/register/Register.hooks.tsx
@@ -1,16 +1,47 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import RegisterSchema from './Register.schema';
+import { getToastDataFromError, showToast } from '../../shared/toast/Toast.service';
+import { useMutation } from '@tanstack/react-query';
+import api from '../../../services/Api.service';
+import { VariantsEnum } from '../../../enums';
+import { TRegisterData } from './Register.types';
 
 export const useRegister = () => {
+  const registerMutation = useRegisterMutation();
   const formData = useForm({
     resolver: yupResolver(RegisterSchema),
     mode: 'all',
   });
 
   const onSubmit = formData.handleSubmit((data) => {
-    console.log(data);
+    registerMutation.mutate(
+      data,
+      {
+        onSuccess: () => {
+          formData.reset();
+        },
+      }
+    );
   });
 
   return { formData, onSubmit };
 };
+
+const useRegisterMutation = () => {
+  return useMutation({
+    mutationKey: ['register'],
+    mutationFn: (data: TRegisterData) => api.post('/auth/register', data),
+    onSuccess: () => {
+      showToast({
+        message: 'register_success',
+        variant: VariantsEnum.SUCCESS,
+      });
+    },
+    onError: (error) => {
+      const toastData = getToastDataFromError(error);
+
+      showToast(toastData);
+    },
+  })
+}
