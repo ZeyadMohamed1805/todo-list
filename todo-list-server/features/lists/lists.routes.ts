5f98e23f0547a37fa1c675016195834213e3b406
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2025-04-24 11:17:18 +0200

    Completed the delete todo list endpoint

--- todo-list-server/features/lists/lists.routes.ts
+++ todo-list-server/features/lists/lists.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { authenticateUser } from "../../middlewares/auth.middleware";
-import { createTodoList, getTodoLists } from "./lists.controller";
+import { createTodoList, deleteTodoList, getTodoLists } from "./lists.controller";
 const router = express.Router();
 
 router.get("/", authenticateUser, getTodoLists);
 router.post("/", authenticateUser, createTodoList);
+router.delete("/:todoListId", authenticateUser, deleteTodoList);
 
 export default router;
