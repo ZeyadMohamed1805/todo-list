9bbf5112733729f4f3fbcf64b73219371b80974b
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2025-04-25 19:18:52 +0300

    Added 404 screen translations

--- todo-list-client/src/components/notFound/homeRedirect/HomeRedirect.component.tsx
+++ todo-list-client/src/components/notFound/homeRedirect/HomeRedirect.component.tsx
@@ -4,12 +4,12 @@ import { useTranslation } from "react-i18next";
 
 const HomeRedirect = () => {
     const navigate = useNavigate();
-    const { i18n: { language }} = useTranslation();
+    const { t, i18n: { language }} = useTranslation();
 
     return (
         <div className={styles.content}>
             <button type='button' className={styles.button} onClick={() => navigate(`/${language}/home`)}>
-                Return Home
+                {t("return_home")}
             </button>
         </div>
     );
--- todo-list-client/src/components/notFound/illustration/Illustration.component.tsx
+++ todo-list-client/src/components/notFound/illustration/Illustration.component.tsx
@@ -1,9 +1,12 @@
+import { useTranslation } from "react-i18next";
 import styles from "./Illustration.module.scss";
 
 const Illustration = () => {
+    const { t } = useTranslation();
+    
     return (
         <div className={styles.illustration}>
-            <img src="/images/not-found.png" alt="404 Not Found" />
+            <img src="/images/not-found.png" alt={t("404_not_found")} />
         </div>
     );
 };
